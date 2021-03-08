272063ae4f80d0879a6ee367c144cd251b6055f2
victor <victornicorios@gmail.com>
2021-03-08 18:29:12 -0300

    Esqueleto HTML

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cors = require('cors')
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
-//var personajesRouter = require('./routes/personajes');
+var usersRouter = require('./routes/users');
+var personajesRouter = require('./routes/personajes');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
-//app.use('/personajes', personajesRouter);
+app.use('/users', usersRouter);
+app.use('/personajes', personajesRouter);
 
 module.exports = app;
